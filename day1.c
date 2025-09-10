6c5f87a1b6f22b49fe0e0ef82003cbb7649e065d
namangoindwani03-cpu <namangoindwani03@gmail.com>
2025-09-10 11:45:14 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,18 @@
+// Q1: Write a program to input two numbers and display their sum.
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    
+    printf("enter first no");
+    scanf("%d",&a);
+    
+    printf("enter second no");
+    scanf("%d",&b);
+    
+    c=a+b;
+    printf("sum is %d\n",c);
+    
+    return 0;
+
+}
